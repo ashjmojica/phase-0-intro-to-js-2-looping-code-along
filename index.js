76335c9cb0f2faa4b90b242acbfad5222dfa7748
Ashley Mojica <ashjmojica@gmail.com>
2022-12-18 21:11:57 -0600

    commit tree bd1b0d3d35c17c4db2f7b30e15fe779338431cb0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+  function writeCards(names) {
+    var arr = []
+  for (let i = 0; i < names.length; i++) {
+    arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+  }
+
+  return arr
+}
+
+writeCards(names);
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function countDown(num){
+
+while (num > -1) {
+
+console.log(num);
+
+num--;
+}
+}
